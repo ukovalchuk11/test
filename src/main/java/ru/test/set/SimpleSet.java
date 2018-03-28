ad09d85a28d71380853865f60f849238e4858435
uriy <pirat2000@ukr.net>
2018-03-28 20:29:14 +0300

    1. Реализовать коллекцию Set на массиве

--- src/main/java/ru/test/set/SimpleSet.java
+++ src/main/java/ru/test/set/SimpleSet.java
@@ -3,29 +3,36 @@ package ru.test.set;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleSet<E> implements Iterable<E> {
 
     private Object[] container = new Object[10];
     private int index = 0;
-
-
+    private boolean value = true;
 
     public void add(E e) {
-        boolean value = true;
+
         if (container.length <= index) {
             container = Arrays.copyOf(container, index * 2);
         }
-        for (int i = 0; i < container.length; i++) {
-            if (e.equals(container[i])) {
-                value = false;
-                break;
-            }
-        }
-        if (value) {
+        contain(e);
+        if(value) {
             container[index++] = e;
         }
+    }
 
+    private void contain(E e) {
+
+        for (int i = 0; i < index; i++) {
+            while (container[i] != null) {
+                if (container[i].equals(e)) {
+                    value = false;
+                    break;
+                }
+
+            }
+        }
     }
 
     @Override
