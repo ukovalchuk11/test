a9cc2ec9ae0f3dd9abae2537d06bef6526608673
uriy <pirat2000@ukr.net>
2018-04-22 21:01:46 +0300

    2. Не перекрывать equals hashCode

--- src/main/java/ru/test/map/UserMap.java
+++ src/main/java/ru/test/map/UserMap.java
@@ -1,4 +1,22 @@
 package ru.test.map;
 
 public class UserMap {
+    private int id;
+    private String mame;
+    private int age;
+
+    public UserMap(int id, String mame, int age) {
+        this.id = id;
+        this.mame = mame;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserMap{" +
+                "id=" + id +
+                ", mame='" + mame + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/test/java/map/UserMapClass.java
+++ src/test/java/map/UserMapClass.java
@@ -1,4 +1,19 @@
 package map;
 
+import org.junit.Test;
+import ru.test.map.UserMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserMapClass {
+    @Test
+    public void showMapNotEqualsAndHashCode(){
+        UserMap user1 = new UserMap(5,"Petr",20);
+        UserMap user2 = new UserMap(5,"Petr",20);
+        Map<UserMap,Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+    }
 }
